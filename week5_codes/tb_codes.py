0e1bfba4f5c99d1bc40fef9e6299fe91e0daaaae
cs_duyy <duyy@uchonor.com>
2025-04-01 17:33:27 +0800

    第五周作业更新-tensorboard使用

--- week5_codes/tb_codes.py
+++ week5_codes/tb_codes.py
@@ -1,39 +1,55 @@
-# import torch
-# import torchvision
 from torch.utils.tensorboard import SummaryWriter
-# from torchvision import datasets, transforms
-
-# # Writer will output to ./runs/ directory by default
-writer = SummaryWriter()
-
-# transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-# trainset = datasets.MNIST('mnist_train', train=True, download=True, transform=transform)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-# model = torchvision.models.resnet50(False)
-# # Have ResNet model take in grayscale rather than RGB
-# model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-# images, labels = next(iter(trainloader))
-
-# grid = torchvision.utils.make_grid(images)
-# writer.add_image('images', grid, 0)
-# writer.add_graph(model, images)
-# writer.close()
-
-
-import keyword
+import numpy as np
+from torchvision import transforms,datasets 
 import torch
-meta = []
-while len(meta)<100:
-    meta = meta+keyword.kwlist # get some strings
-meta = meta[:100]
+import torchvision
 
-for i, v in enumerate(meta):
-    meta[i] = v+str(i)
+# 随机数
+writer = SummaryWriter('runs/random_data')
+for n_iter in range(100):
+    writer.add_scalar('Loss/train', np.random.random(), n_iter)
+    writer.add_scalar('Loss/test', np.random.random(), n_iter)
+    writer.add_scalar('Accuracy/train', np.random.random(), n_iter)
+    writer.add_scalar('Accuracy/test', np.random.random(), n_iter)
+writer.close()
 
-label_img = torch.rand(100, 3, 10, 32)
+# 直线
+writer = SummaryWriter('runs/linear_data')
+x = range(100)
+for i in x:
+    writer.add_scalar('y=2x', i * 2, i)
+writer.close()
+
+# 曲线
+writer = SummaryWriter('runs/curve_data')
+r = 5
 for i in range(100):
-    label_img[i]*=i/100.0
+    writer.add_scalars('run_14h', {'xsinx':i*np.sin(i/r),
+                                    'xcosx':i*np.cos(i/r),
+                                    'tanx': np.tan(i/r)}, i)    
+writer.close()
 
-writer.add_embedding(torch.randn(100, 5), metadata=meta, label_img=label_img)
-writer.add_embedding(torch.randn(100, 5), label_img=label_img)
-writer.add_embedding(torch.randn(100, 5), metadata=meta)
+# 立体
+writer = SummaryWriter('runs/histogram_data')    
+for i in range(10):
+    x = np.random.random(1000)
+    writer.add_histogram('distribution centers', x + i, i)
+    
+writer.close()
+
+# 计算图-不好理解-again
+writer = SummaryWriter('runs/image_data')
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+trainset = datasets.MNIST('mnist_train', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+images, labels = next(iter(trainloader))
+grid = torchvision.utils.make_grid(images)
+writer.add_image('images', grid, 0)
+
+model = torchvision.models.resnet50(False)
+# Have ResNet model take in grayscale rather than RGB
+model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+writer.add_graph(model, images)
+
+writer.close()
